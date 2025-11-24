96fa27a1ba912d1c0d6a4c9338d36216a942cd28
Jayashree727 <rkrupeshkumar@gmail.com>
2025-11-24 14:02:54 +0530

    added prime function

--- learning.py
+++ learning.py
@@ -13,3 +13,12 @@ else:
 
 def test_login():
     print("login")
+
+def prime_numbers(self):
+    for i in range(2, 50):
+        for j in range(2, i):
+            if i%j==0:
+                break
+        else:
+            print(i)
+            
